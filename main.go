26fb5a2b07051f56858409cc9d16b5c3ee729cc7
Tamura Yukichi <tamura.yukichi@gmail.com>
2018-12-03 16:36:58 +0900

    [from now] 2018/12/03 16:36:58
     db.go   |  37 -----------------
     main.go | 138 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     2 files changed, 138 insertions(+), 37 deletions(-)

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Tenant struct {
+	gorm.Model
+	Name string
+}
+
+type Product struct {
+	gorm.Model
+	TenantID uint
+	Title    string
+	Price    uint
+}
+
+func main() {
+	db, err := gorm.Open("postgres", "user=xuser password=xpass dbname=xdb sslmode=disable")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	err = db.DB().Ping()
+	if err != nil {
+		panic("failed to ping")
+	}
+
+	setup(db)
+
+	repo, err := newRepo(db)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read
+	product, err := repo.findProduct(3)
+	if err != nil {
+		panic(err)
+	}
+	p(product, "Read")
+
+	// Update
+	err = repo.updateProduct(product)
+	if err != nil {
+		panic(err)
+	}
+	p(product, "Update")
+
+	// Delete
+	err = repo.deleteProduct(3)
+	if err != nil {
+		panic(err)
+	}
+	_, err = repo.findProduct(3)
+	if err != nil {
+		if err.Error() == "record not found" {
+			fmt.Println("Deleted successfully")
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func setup(db *gorm.DB) {
+	// Clear
+	db.DropTableIfExists(&Tenant{})
+	db.DropTableIfExists(&Product{})
+
+	// Migrate the schema
+	db.AutoMigrate(&Tenant{})
+	db.AutoMigrate(&Product{})
+
+	// Create
+	{
+		tenant := &Tenant{Name: "Apple"}
+		db.Create(tenant)
+		db.Create(&Product{TenantID: tenant.ID, Title: "Macbook Pro", Price: 250000})
+		db.Create(&Product{TenantID: tenant.ID, Title: "iPhoneX", Price: 120000})
+	}
+	{
+		tenant := &Tenant{Name: "Google"}
+		db.Create(tenant)
+		db.Create(&Product{TenantID: tenant.ID, Title: "Pixel3", Price: 140000})
+	}
+	{
+		tenant := &Tenant{Name: "Amazon"}
+		db.Create(tenant)
+		db.Create(&Product{TenantID: tenant.ID, Title: "Amazon echo", Price: 4500})
+		db.Create(&Product{TenantID: tenant.ID, Title: "Amazon fireTV", Price: 6000})
+		db.Create(&Product{TenantID: tenant.ID, Title: "Amazon mini", Price: 2000})
+	}
+}
+
+type repo struct {
+	*gorm.DB
+}
+
+func newRepo(db *gorm.DB) (*repo, error) {
+	return &repo{db}, nil
+}
+
+func (db *repo) findProduct(id uint) (*Product, error) {
+	var product Product
+	err := db.First(&product, 3).Error
+	return &product, err
+}
+
+func (db *repo) createProduct(product *Product) error {
+	return db.Create(product).Error
+}
+
+func (db *repo) updateProduct(product *Product) error {
+	return db.Model(&product).Update("Price", 99999).Error
+}
+
+func (db *repo) deleteProduct(id uint) error {
+	product := new(Product)
+	product.ID = id
+	return db.Delete(product).Error
+}
+
+func p(v interface{}, msgs ...string) {
+	var msg string
+	if len(msgs) > 0 {
+		msg = msgs[0]
+	}
+	fmt.Printf("%s %+v\n", msg, func(v interface{}) string {
+		b, _ := json.MarshalIndent(v, "-", "\t")
+		return string(b)
+	}(v))
+}
