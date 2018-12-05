4f9edea3bc5d35a3342a1bf33747ed41a0158ecc
Tamura Yukichi <tamura.yukichi@gmail.com>
2018-12-05 13:17:15 +0900

    [from now] 2018/12/05 13:17:15
     main.go | 156 +++++++++++++++++++++++++++++++++++++++++-----------------------
     1 file changed, 100 insertions(+), 56 deletions(-)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -10,6 +11,7 @@ import (
 
 type Tenant struct {
 	gorm.Model
+	Key  string
 	Name string
 }
 
@@ -36,38 +38,74 @@ func main() {
 
 	setup(db)
 
-	//repo, err := newRepo(db)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// Read
-	//product, err := repo.findProduct(3)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//p(product, "Read")
-	//
-	//// Update
-	//err = repo.updateProduct(product)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//p(product, "Update")
-	//
-	//// Delete
-	//err = repo.deleteProduct(3)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//_, err = repo.findProduct(3)
-	//if err != nil {
-	//	if err.Error() == "record not found" {
-	//		fmt.Println("Deleted successfully")
-	//	} else {
-	//		panic(err)
-	//	}
-	//}
+	// ===== Start application-like =====
+
+	var tenants []Tenant
+	err = db.Find(&tenants).Error
+	if err != nil {
+		panic(err)
+	}
+	p(tenants, "Read all tenants (as super user)")
+
+	// Create connections for each tenants
+	conns := make(map[uint]*gorm.DB, len(tenants)) // map[tenantID]*gorm.DB // TODO: consider connection num
+	for _, tenant := range tenants {
+		udb, err := gorm.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=xdb sslmode=disable", tenant.Key, tenant.Key))
+		if err != nil {
+			log.Println(err)
+			panic(fmt.Sprintf("failed to connect database as %s", tenant.Key))
+		}
+		conns[tenant.ID] = udb
+	}
+	defer func() {
+		for tid, udb := range conns {
+			fmt.Printf("Close db connection: tenant_id: %d\n", tid)
+			err := udb.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}()
+
+	// Initialize single repo for multi tenants
+	repo, err := newRepo(conns)
+	if err != nil {
+		panic(err)
+	}
+
+	// Test commands to see RLS working correctly
+	for _, tenant := range tenants {
+		// a user of a tenant is accessing our application right now...
+
+		// Read
+		products, err := repo.findAllProducts(tenant.ID)
+		if err != nil {
+			panic(err)
+		}
+		p(products, fmt.Sprintf("Read all products of tenant. tid: %d, tname: %s", tenant.ID, tenant.Name))
+
+		//// Update
+		//err = repo.updateProduct(product)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//p(product, "Update")
+		//
+		//// Delete
+		//err = repo.deleteProduct(1)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//_, err = repo.findProduct(1)
+		//if err != nil {
+		//	if err.Error() == "record not found" {
+		//		fmt.Println("Deleted successfully")
+		//	} else {
+		//		panic(err)
+		//	}
+		//}
+
+	}
 }
 
 func cleanup(db *gorm.DB) {
@@ -91,9 +129,9 @@ func setup(db *gorm.DB) {
 	db.Exec("ALTER TABLE products ENABLE ROW LEVEL SECURITY;")
 
 	// Create roles(users)
-	db.Exec("CREATE ROLE apple WITH LOGIN")  // Apple
-	db.Exec("CREATE ROLE google WITH LOGIN") // Google
-	db.Exec("CREATE ROLE amazon WITH LOGIN") // Amazon
+	db.Exec("CREATE ROLE apple WITH LOGIN PASSWORD 'apple'")   // Apple
+	db.Exec("CREATE ROLE google WITH LOGIN PASSWORD 'google'") // Google
+	db.Exec("CREATE ROLE amazon WITH LOGIN PASSWORD 'amazon'") // Amazon
 
 	// Create policies
 	// TODO: role=apple$1 みたいにして USING(id = EXTRACT_ID(current_user)) みたいなことできればポリシー一つで済むようになる
@@ -128,18 +166,18 @@ func setup(db *gorm.DB) {
 
 	// Create records
 	{
-		tenant := &Tenant{Name: "Apple"}
+		tenant := &Tenant{Name: "Apple, Inc", Key: "apple"}
 		db.Create(tenant) // id:1
 		db.Create(&Product{TenantID: tenant.ID, Title: "Macbook Pro", Price: 250000})
 		db.Create(&Product{TenantID: tenant.ID, Title: "iPhoneX", Price: 120000})
 	}
 	{
-		tenant := &Tenant{Name: "Google"}
+		tenant := &Tenant{Name: "Google, Inc", Key: "google"}
 		db.Create(tenant) // id:2
 		db.Create(&Product{TenantID: tenant.ID, Title: "Pixel3", Price: 140000})
 	}
 	{
-		tenant := &Tenant{Name: "Amazon"}
+		tenant := &Tenant{Name: "Amazon, Inc", Key: "amazon"}
 		db.Create(tenant) // id:3
 		db.Create(&Product{TenantID: tenant.ID, Title: "Amazon echo", Price: 4500})
 		db.Create(&Product{TenantID: tenant.ID, Title: "Amazon fireTV", Price: 6000})
@@ -148,32 +186,38 @@ func setup(db *gorm.DB) {
 }
 
 type repo struct {
-	*gorm.DB
+	conns map[uint]*gorm.DB
 }
 
-func newRepo(db *gorm.DB) (*repo, error) {
-	return &repo{db}, nil
+func newRepo(conns map[uint]*gorm.DB) (*repo, error) {
+	return &repo{conns}, nil
 }
 
-func (db *repo) findProduct(id uint) (*Product, error) {
+func (db *repo) findAllProducts(tenantID uint) ([]Product, error) {
+	var products []Product
+	err := db.conns[tenantID].Find(&products).Error
+	return products, err
+}
+
+func (db *repo) findProduct(tenantID, id uint) (*Product, error) {
 	var product Product
-	err := db.First(&product, 3).Error
+	err := db.conns[tenantID].First(&product, 3).Error
 	return &product, err
 }
 
-func (db *repo) createProduct(product *Product) error {
-	return db.Create(product).Error
-}
-
-func (db *repo) updateProduct(product *Product) error {
-	return db.Model(&product).Update("Price", 99999).Error
-}
-
-func (db *repo) deleteProduct(id uint) error {
-	product := new(Product)
-	product.ID = id
-	return db.Delete(product).Error
-}
+//func (db *repo) createProduct(product *Product) error {
+//	return db.Create(product).Error
+//}
+//
+//func (db *repo) updateProduct(product *Product) error {
+//	return db.Model(&product).Update("Price", 99999).Error
+//}
+//
+//func (db *repo) deleteProduct(id uint) error {
+//	product := new(Product)
+//	product.ID = id
+//	return db.Delete(product).Error
+//}
 
 func p(v interface{}, msgs ...string) {
 	var msg string
